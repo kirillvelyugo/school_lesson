1fbef0b430ebefc50404466119e27928a3524092
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-09-28 21:30:37 +0500

    add lesson 28.09

---
+++ Урок 28.09/Решения/Задание 14.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> a(1000, vector<int>(1000, -1000));
+
+int game(int x, int y) {
+    if (a[x][y] != -1000) {
+        return a[x][y];
+    }
+    if (x + y >= 74){
+        return a[x][y] = 0;
+    }
+
+    vector <int> tmp = { game(x + 2,y),game(x * 2,y),game(x,y + 2),game(x,y * 2) }; 
+    vector<int>neg;
+    int res = 0;
+
+    for (int i = 0; i < tmp.size(); i++) {
+        if (tmp[i] <= 0) {
+            neg.push_back(tmp[i]);
+        }
+    }
+    
+    if (!neg.empty()) {
+        res = *max_element(neg.begin(), neg.end());
+        res = -res + 1;
+    }
+    else {
+        res = -*max_element(tmp.begin(), tmp.end());
+    }
+    return a[x][y]=res;
+}
+
+int main()
+{
+    int x = 7;
+    for (int i = 1; i <= 66; i++) {
+        int r = game(x, i);
+        if (r == 1) {
+            cout << '(' << x << ',' << i << ") " << r << endl;
+        }
+        if (r == 2) {
+            cout << '(' << x << ',' << i << ") " << r << endl;
+        }
+        if (r == -2) {
+            cout << '(' << x << ',' << i << ") " << r << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ Урок 28.09/Решения/Задание 43.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> a(10000, -1000);
+
+int game(int x) {
+	if (a[x] != -1000) {
+		return a[x];
+	}
+	if (x >= 34) {
+		return a[x] = 0;
+	}
+
+	vector <int> tmp = { game(x + 1), game(x + 2), game(x + 3), game(x * 2) };
+	vector <int> neg;
+	int res = 0;
+
+	for (int i = 0; i < tmp.size(); i++) {
+		if (tmp[i] <= 0) {
+			neg.push_back(tmp[i]);
+		}
+	}
+
+	if (!neg.empty()) {
+		res = *max_element(neg.begin(), neg.end());
+		res = -res + 1;
+	}
+	else {
+		res = -*max_element(tmp.begin(), tmp.end());
+	}
+
+	return a[x] = res;
+}
+
+int main()
+{
+	for (int x = 1; x <= 33; x++) {
+		int res = game(x);
+		if (res == 1) {
+			cout << '(' << x << ") " << res << endl;
+		}
+		if (res == 2) {
+			cout << '(' << x << ") " << res << endl;
+		}
+		if (res == -2) {
+			cout << '(' << x << ") " << res << endl;
+		}
+	}
+	return 0;
+}
